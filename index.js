eb888393f9dbd82d421d3877309c781559a37c6b
Lydia Wagner <lydiawagner@gmail.com>
2016-07-30 09:31:43 -0400

    Add cors, listen locally

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+    path: '/socket/socket.io'
+})
+var cors = require('cors')
+
+app.use(cors());
+
+app.all('/*', function(req, res, next) {
+    console.log('all');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.get('/', function(req, res) {
+    console.log('get');
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
+    console.log('on');
+
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+port = process.env.PORT || 3000;
+
+http.listen(port, function() {
+    console.log('listening on ' + port);
 });
