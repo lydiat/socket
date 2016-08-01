d29429372ccaf2d878e9da0f199f9c7996bbbcfd
Lydia Wagner <lydiawagner@gmail.com>
2016-08-01 15:24:27 -0400

    abstracting domain, disconnect message

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var express = require('express');
-var app     = express();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, {
     path: '/socket/socket.io'
 })
 
 app.get('/', function(req, res) {
-    console.log('get');
+    console.log('app.get');
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
+
+    console.log('io.on connection');
     io.emit('chat message', 'A user has joined');
+
+    socket.on('disconnect', function() {
+        io.emit('chat message', 'A user has left');
+    });
 
     socket.on('chat message', function(msg) {
         console.log(msg);
